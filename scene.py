3cea0099c65ec48b29f1a5e1e722be54642d6eed
M# <leftomash@gmail.com>
2024-06-21 17:10:43 +0530

    Made a beating heart animation with a message

--- scene.py
+++ scene.py
@@ -1,5 +1,92 @@
 from manim import *
 
+class BeatingHeartWithMessage(Scene):
+    def construct(self):
+        # initial two beats
+        self.beat(0)
+        self.beat(0)
+
+        # moves to the side
+        self.move(3.5)
+        
+        # beats to the side
+        self.beat(3.5)
+        
+        # make text
+        text_one = Text("This is my heart, ").shift(3 * LEFT).shift(1 * UP)
+        arrow = Arrow(start=LEFT, end=RIGHT).next_to(text_one, RIGHT)
+
+        heart = FunctionGraph(
+            lambda x: (x ** 2) ** (1 / 3) + (3.3 - x ** 2) ** 0.5 * np.sin(7 * PI * x),
+            x_range=[-1.8, 1.8],
+            color=RED,
+        ).shift(3.5 * RIGHT)
+
+        self.add(heart)
+        self.play(
+            Write(text_one),
+            run_time=0.5,
+        )
+        self.play(
+            Create(arrow),
+            run_time=0.1,
+        )
+        self.remove(heart)
+
+        # heart beating
+        self.beat(3.5)
+
+        # second text
+        text_two = Text("and it is all yours!").next_to(text_one, DOWN)
+
+        self.add(heart)
+        self.play(
+            Write(text_two),
+            run_time=0.5,
+        )
+        self.remove(heart)
+
+        # last two beats
+        self.beat(3.5)
+        self.beat(3.5)
+
+
+    def beat(self, n):
+        for a in range(70, 150, 1):
+            heart = FunctionGraph(
+                lambda x: (x ** 2) ** (1 / 3) + (3.3 - x ** 2) ** 0.5 * np.sin(a / 10 * PI * x),
+                x_range=[-1.8, 1.8],
+                color=RED,
+            ).shift(n * RIGHT)
+
+            self.add(heart)
+            self.wait(1 / 30)
+            self.remove(heart)
+        
+        for a in range(150, 70, -1):
+            heart = FunctionGraph(
+                lambda x: (x ** 2) ** (1 / 3) + (3.3 - x ** 2) ** 0.5 * np.sin(a / 10 * PI * x),
+                x_range=[-1.8, 1.8],
+                color=RED,
+            ).shift(n * RIGHT)
+
+            self.add(heart)
+            self.wait(1 / 30)
+            self.remove(heart)
+
+
+    def move(self, n):
+        heart = FunctionGraph(
+            lambda x: (x ** 2) ** (1 / 3) + (3.3 - x ** 2) ** 0.5 * np.sin(7 * PI * x),
+            x_range=[-1.8, 1.8],
+            color=RED,
+        )
+
+        self.play(
+            heart.animate.shift(n * RIGHT)
+        )
+
+
 class BeatingHeart(Scene):
     def construct(self):
         for a in range(70, 150, 1):
@@ -12,8 +99,6 @@ class BeatingHeart(Scene):
             self.add(heart)
             self.wait(1 / 30)
             self.remove(heart)
-
-        self.wait()
 
         for a in range(150, 70, -1):
             heart = FunctionGraph(
